d1de3eb3ff0e08556b251edb1e0f8e0a60895914
treddy08 <treddy@redhat.com>
2022-07-15 16:12:54 +1000

    Install kamel k

--- envvars/terminal.sh
+++ envvars/terminal.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
 # Environment variables to set for the terminal
-
-curl -L https://github.com/apache/camel-k/releases/download/v1.9.2/camel-k-client-1.9.2-linux-64bit.tar.gz | tar xzvf -
-mv kamel /usr/local/bin
